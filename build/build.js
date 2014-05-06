c792d08eb3257f662d8e876daac77a8a2af1cfee
Riplexus <Riplexus@googlemail.com>
2014-05-06 09:55:46 +0200

    get templates and views from subdirectories for building

--- build/build.js
+++ build/build.js
@@ -1,41 +1,49 @@
 var fs = require('fs'),
+    walk = require('./walk'),
     root = __dirname+'/../../../',
     mainConfig = require('../../../js/main.js'),
     target = 'configs/dXBuild.min.js',
     viewFolder = 'js/views',
-    views,
     templateFolder = 'templates',
+    views,
     templates,
     includes = [],
     result;
 
-views = fs.readdirSync(root+viewFolder);
-includes = includes.concat(views.map(function(v) {
-    return 'views/'+v.replace('.js', '');
-}));
+//views = fs.readdirSync(root+viewFolder);
+walk(root+viewFolder, function(err, views) {
+    if (err) { throw err; }
 
-templates = fs.readdirSync(root+templateFolder);
-includes = includes.concat(templates.map(function(v) {
-    return 'text!templates/'+v.replace('.js', '');
-}));
+    includes = includes.concat(views.map(function(file) {
+        return file.replace(/(.*\/js\/views)/, 'views');
+    }));
 
-result = {
-    baseUrl: '../js',
+    walk(root+templateFolder, function(err, templates) {
+        if (err) { throw err; }
 
-    name: 'main',
-    out: '../js/main-build.js',
-    //optimize: 'none',
-    findNestedDependencies: true,
-    preserveLicenseComments: false,
-    "insertRequire": ["main"],
-    include: includes,
-    uglify: { max_line_length: 500 },
+        includes = includes.concat(templates.map(function(file) {
+            return 'text!'+file.replace(/(.*\/templates)/, 'templates');
+        }));
 
-    paths: mainConfig.paths,
-    shim: mainConfig.shim,
+        result = {
+            baseUrl: '../js',
 
-    // Enforce define to catch 404 errors in IE
-    enforceDefine: true
-};
+            name: 'main',
+            out: '../js/main-build.js',
+            //optimize: 'none',
+            findNestedDependencies: true,
+            preserveLicenseComments: false,
+            "insertRequire": ["main"],
+            include: includes,
+            uglify: { max_line_length: 500 },
 
-fs.writeFileSync(root+target, '('+JSON.stringify(result)+')');
+            paths: mainConfig.paths,
+            shim: mainConfig.shim,
+
+            // Enforce define to catch 404 errors in IE
+            enforceDefine: true
+        };
+
+        fs.writeFileSync(root+target, '('+JSON.stringify(result)+')');
+    });
+});
---
+++ build/walk.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+/**
+ *
+ * @param dir
+ * @param done
+ * @source http://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
+ */
+
+module.exports = function walk(dir, done) {
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+        var i = 0;
+        (function next() {
+            var file = list[i++];
+            if (!file) return done(null, results);
+            file = dir + '/' + file;
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function(err, res) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else {
+                    results.push(file);
+                    next();
+                }
+            });
+        })();
+    });
+};
