fadfe9c6c2949b2ecf66665487074780552ec4d7
Riplexus <Riplexus@googlemail.com>
2015-04-13 15:20:12 +0200

    relative links, added editorconfig

--- build/link.js
+++ build/link.js
@@ -10,9 +10,9 @@ fs.readFile('./index.html', 'utf8', function (err,data) {
     var result;
     
     if (isBuild) {
-        result = data.replace(/data-main="\/js\/main"/g, 'data-main="/js/main-build"');
+        result = data.replace(/data-main="js\/main"/g, 'data-main="js/main-build"');
     } else {
-        result = data.replace(/data-main="\/js\/main-build"/g, 'data-main="/js/main"');
+        result = data.replace(/data-main="js\/main-build"/g, 'data-main="js/main"');
     }
 
     fs.writeFile('./index.html', result, 'utf8', function (err) {
